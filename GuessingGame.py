86af5f9e060b2dbac70cd5bc360f7dda58a82c3c
Eshaan <eshaangupta.in@gmail.com>
2021-10-02 13:49:51 +0530

    initial commit

---
+++ GuessingGame.py
@@ -0,0 +1,24 @@
+import random
+
+number = random.randrange(10)
+guesses_left = 5
+
+print("Guess The Number!")
+state = "none" 
+while guesses_left > 0:
+        guess = int(input("Guess A Number : "))
+        guesses_left -= 1
+
+        if(guess < number):
+            print("Guess a Number Higher Than ", guess)
+
+        if(guess > number):
+            print("Guess a Number Lower Than ", guess)
+
+        if(guess == number):
+            print("You Won!")
+            state = "win"
+            guesses_left = 0
+
+        if (guesses_left <= 0 and state != "win"):
+            print("You Loose!")
